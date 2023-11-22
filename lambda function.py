0e7ae7110946341ae36bdf74c7d103a0f37843b5
SergeyIvchenkov <130689220+SergeyIvchenkov@users.noreply.github.com>
2023-11-22 19:53:23 +0700

    Update lambda function.py

--- lambda function.py
+++ lambda function.py
@@ -3,6 +3,7 @@ import pandas as pd
 import urllib.parse
 import os
 
+# Создаем переменные окружения
 os_input_s3_cleansed_layer = os.environ['s3_cleansed_layer']
 os_input_glue_catalog_db_name = os.environ['glue_catalog_db_name']
 os_input_glue_catalog_table_name = os.environ['glue_catalog_table_name']
@@ -10,18 +11,18 @@ os_input_write_data_operation = os.environ['write_data_operation']
 
 
 def lambda_handler(event, context):
-    # Get the object from the event and show its content type
+    # Достаем через содержание события s3 имя бакета и ключ
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
     try:
 
-        # Creating DF from content
+        # Создаем датафрейм
         df_raw = wr.s3.read_json('s3://{}/{}'.format(bucket, key))
 
-        # Extract required columns:
+        # Вытаскиваем только значение из ключа items:
         df_step_1 = pd.json_normalize(df_raw['items'])
 
-        # Write to S3
+        # Запись в s3
         wr_response = wr.s3.to_parquet(
             df=df_step_1,
             path=os_input_s3_cleansed_layer,
